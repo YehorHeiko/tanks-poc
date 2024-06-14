cf0811c41dd7eff2b82b3a4af3c6daee8a05335c
Yehor Heiko <yehorheiko.developer@gmail.com>
2024-06-14 22:07:38 +0300

    Update to Render

--- script.js
+++ script.js
@@ -1,59 +1,57 @@
 const field = document.querySelector('.field')
-const cell = document.querySelector('.cell')
-let tankCell 
+const baseCell = document.querySelector('.cell')
+const gameState = {x: 0, y: 0, dir:'S'}
+const rowLength = 22
+const rowCount = 13
+const cellCount = rowLength * rowCount
+const {min, max} = Math
+const dirs = {
+    N: 'up', E: 'right', S: 'down', W: 'left',
+    ArrowUp: 'N', ArrowRight: 'E', ArrowDown: 'S', ArrowLeft: 'W',
+    n: {x: 0, y: -1}, e: {x: 1, y: 0}, s: {x: 0, y: 1}, w:{x: -1, y: 0},
+}
 
-buildField()
-createTank(5, 5)
-assignListener()
+render()
+assignKeyHandler()
 
+function render() {
+    const index = coordsToIndex(gameState.x, gameState.y)
 
-function buildField() {
-    for (let i = 0; i < 285; i++) {
-        addCells();
+    field.innerHTML = ''
+    
+    for (let i = 0; i < cellCount; i++) {
+        const cell = baseCell.cloneNode()
 
+        if (i == index) {
+            cell.classList.add('tank', dirs[gameState.dir])
+        }
+        field.append(cell)
     }
 }
 
-
-function addCells() {
-    const createCell = cell.cloneNode()
-    field.append(createCell)
+function coordsToIndex(x, y) {
+    return x + y * rowLength
 }
 
-function createTank(x, y) {
-    const i = y * 22 + x
-    tankCell = field.children[i]
-    tankCell.classList.add('tank')
-}
+function assignKeyHandler() {
+    onkeydown = e => {
+        const dir = dirs[e.key]
 
-function assignListener() {
-    document.addEventListener('keydown', (e) => {
-        const {x, y} = getCoords(tankCell)
-        if (e.code == 'ArrowUp' && y > 0) {
-            moveTank(x, y - 1)
+        if (dir) {
+            gameState.dir = dir
+            moveTank()
+            render()
         }
-        if (e.code == 'ArrowLeft' && x > 0) {
-            moveTank(x - 1, y)
-        }
-        if (e.code == 'ArrowRight' && x < 21) {
-            moveTank(x + 1, y)
-        }
-        if (e.code == 'ArrowDown' && y < 12) {
-            moveTank(x, y + 1)
-        }
-    })
+    }
 }
 
-function getCoords(cell) {
-    const cells = [...field.children]
-    const i = cells.indexOf(cell)
-    const x = i % 22
-    const y = Math.trunc(i / 22)
-    return { x, y }
+function moveTank() {
+    const {x, y} = dirs[gameState.dir.toLowerCase()]
+    
+    gameState.x = min(rowLength - 1, max(0, gameState.x + x))
+    gameState.y = min(rowCount - 1, max(0, gameState.y + y))
 }
 
-function moveTank(x, y) {
-    tankCell.classList.remove('tank')
-    createTank(x, y)
+function shootTank(){
+    
 }
-
